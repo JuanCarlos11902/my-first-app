977f5320d4d7fe426dc01e083147fd999e992d40
JuanCarlos11902 <105056958+JuanCarlos11902@users.noreply.github.com>
2023-10-03 17:11:29 +0200

    Ejercicio Acabado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CayetanoComponent } from './cayetano/cayetano.component';
+import { JokeListComponentComponent } from './joke-list-component/joke-list-component.component';
 
 @Component({
   selector: 'app-root',
